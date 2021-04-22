facfd48a0e11df35e2ebd3ef9f13a6cacd076c05
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2021-04-22 11:02:15 -0300

    Feito a configuração de acesso para perfis de usuário

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -50,6 +50,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/actuator").permitAll()
                 .antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
                 .antMatchers(HttpMethod.POST,"/auth").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/topicos/*").hasRole("MODERADOR")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$vaw4bIOWl4//QpuWS9JAKOqQQoF8HHmD3mShLEVhwa5TcZxzVQ.E.');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$vaw4bIOWl4//QpuWS9JAKOqQQoF8HHmD3mShLEVhwa5TcZxzVQ.E.');
+
+INSERT INTO PERFIL(id,nome) values (1,'ROLE_ALUNO');
+INSERT INTO PERFIL(id,nome) values (2,'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) values (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) values (2,2);
+
+
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
